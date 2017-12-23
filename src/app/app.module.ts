cebd399825ad7c8f71cb433d6b94ab05516de596
arindam.gohain <arindam.gohain@cgi.com>
2017-12-23 10:46:11 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { DashboardComponent } from './dashboard.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+   declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+    providers: [
+      HeroService
+    ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+  
+ }
